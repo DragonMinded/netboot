5bd21f891d941bfc7f6c7dd5000ca77729269af7
Jennifer Taylor <dragonminded@dragonminded.com>
2019-09-20 05:27:54 +0000

    Add a hello world route, and a few routes for fetching cabinet information.

--- netboot/web/app.py
+++ netboot/web/app.py
@@ -1,8 +1,65 @@
 import os.path
 import yaml
+import traceback
+from functools import wraps
+from typing import Callable, Dict, Any
 
-from flask import Flask
-from netboot import CabinetManager, DirectoryManager
+from flask import Flask, Response, make_response, jsonify as flask_jsonify
+from netboot import Cabinet, CabinetManager, DirectoryManager
+
+
+app = Flask(__name__)
+
+
+def jsonify(func: Callable) -> Callable:
+    @wraps(func)
+    def decoratedfunction(*args: Any, **kwargs: Any) -> Response:
+        try:
+            return flask_jsonify({**func(*args, **kwargs), "error": False})
+        except Exception as e:
+            print(traceback.format_exc())
+            return flask_jsonify({
+                'error': True,
+                'message': str(e),
+            })
+    return decoratedfunction
+
+
+@app.route('/')
+def home() -> Response:
+    return make_response("Hello, world!", 200)
+
+
+def cabinet_to_dict(cab: Cabinet) -> Dict[str, str]:
+    status, progress = cab.state
+
+    return {
+        'ip': cab.ip,
+        'description': cab.description,
+        'filename': cab.filename,
+        'target': cab.target,
+        'version': cab.version,
+        'status': status,
+        'progress': progress,
+    }
+
+
+@app.route('/cabinets')
+@jsonify
+def cabinets() -> Dict[str, Any]:
+    manager = app.config['CabinetManager']
+    cabinets = manager.cabinets
+    return {
+        'cabinets': [cabinet_to_dict(cab) for cab in cabinets],
+    }
+
+
+@app.route('/cabinets/<ip>')
+@jsonify
+def cabinet(ip: str) -> Dict[str, Any]:
+    manager = app.config['CabinetManager']
+    cabinet = manager.cabinet(ip)
+    return cabinet_to_dict(cabinet)
 
 
 class AppException(Exception):
@@ -37,7 +94,6 @@ def spawn_app(config_file: str) -> Flask:
         if not os.path.isdir(directory):
             raise AppException(f"Invalid YAML file format for {config_file}, {directory} is not a directory!")
 
-    app = Flask(__name__)
     app.config['CabinetManager'] = CabinetManager.from_yaml(cabinet_file)
     app.config['DirectoryManager'] = DirectoryManager(directories)
 
