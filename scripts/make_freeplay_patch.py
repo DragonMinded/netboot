fc0e7272d1b504d9b3a2a67bb7519d1425aa4159
Jennifer Taylor <dragonminded@dragonminded.com>
2019-11-21 06:57:23 +0000

    Check in updated freeplay and no attract patch generators to work for any Naomi ROM.

--- scripts/make_freeplay_patch.py
+++ scripts/make_freeplay_patch.py
@@ -3,9 +3,50 @@ import argparse
 import os
 import os.path
 import sys
+from typing import List, Optional
 
 from netboot import Binary
 from naomi import force_freeplay
+
+
+def legacy_force_freeplay(data: bytes) -> List[str]:
+    # Apply brute-force search for EEPROM parsing routine (doesn't always work).
+    newdata = force_freeplay(data)
+    return ["# Description: force free-play", *Binary.diff(data, newdata)]
+
+
+def standard_force_freeplay(data: bytes) -> List[str]:
+    def _hex(val: int) -> str:
+        out = hex(val)[2:]
+        out = out.upper()
+        if len(out) == 1:
+            out = "0" + out
+        return out
+
+    def _output(offset: int, old: Optional[int], new: int) -> str:
+        return f"{_hex(offset)}: {_hex(old) if old is not None else '*'} -> {_hex(new)}"
+
+    def _make_differences(region: int) -> List[str]:
+        location = 0x1E0 + (0x10 * region)
+        values: List[str] = [
+            _output(location, None, 0x01),
+        ]
+
+        if data[location + 3] != 27:
+            values.append(_output(location + 3, data[location + 3], 27))
+        return values
+
+    # Manually make a differences file since we want to support ROMs that already
+    # have enabled EEPROM overrides.
+    return [
+        "# Description: force free-play",
+        f"# File size: {len(data)}",
+        *_make_differences(0),
+        *_make_differences(1),
+        *_make_differences(2),
+        *_make_differences(3),
+        *_make_differences(4),
+    ]
 
 
 def main() -> int:
@@ -25,6 +66,13 @@ def main() -> int:
         type=str,
         help='Write patches to a file instead of stdout.',
     )
+    parser.add_argument(
+        '--mode',
+        type=str,
+        choices=["legacy", "standard"],
+        default="standard",
+        help="Mode to use when creating the patch. Defaults to standard mode (header modifications only).",
+    )
 
     # Grab what we're doing
     args = parser.parse_args()
@@ -32,8 +80,12 @@ def main() -> int:
     # Grab the rom, parse it
     with open(args.bin, "rb") as fp:
         data = fp.read()
-    newdata = force_freeplay(data)
-    differences = ["# Description: force free-play", *Binary.diff(data, newdata)]
+    if args.mode == "legacy":
+        differences = legacy_force_freeplay(data)
+    elif args.mode == "standard":
+        differences = standard_force_freeplay(data)
+    else:
+        raise Exception(f"Invalid choice {args.mode}")
     if not args.patch_file:
         for line in differences:
             print(line)
--- scripts/make_no_attract_patch.py
+++ scripts/make_no_attract_patch.py
@@ -3,9 +3,50 @@ import argparse
 import os
 import os.path
 import sys
+from typing import List, Optional
 
 from netboot import Binary
 from naomi import force_no_attract_sound
+
+
+def legacy_force_no_attract(data: bytes) -> List[str]:
+    # Apply brute-force search for EEPROM parsing routine (doesn't always work).
+    newdata = force_no_attract_sound(data)
+    return ["# Description: force silent attract mode", *Binary.diff(data, newdata)]
+
+
+def standard_force_no_attract(data: bytes) -> List[str]:
+    def _hex(val: int) -> str:
+        out = hex(val)[2:]
+        out = out.upper()
+        if len(out) == 1:
+            out = "0" + out
+        return out
+
+    def _output(offset: int, old: Optional[int], new: int) -> str:
+        return f"{_hex(offset)}: {_hex(old) if old is not None else '*'} -> {_hex(new)}"
+
+    def _make_differences(region: int) -> List[str]:
+        location = 0x1E0 + (0x10 * region)
+        values: List[str] = [
+            _output(location, None, 0x01),
+        ]
+
+        if (data[location + 1] & 0x2) == 0:
+            values.append(_output(location + 1, data[location + 1], data[location + 1] | 0x2))
+        return values
+
+    # Manually make a differences file since we want to support ROMs that already
+    # have enabled EEPROM overrides.
+    return [
+        "# Description: force silent attract mode",
+        f"# File size: {len(data)}",
+        *_make_differences(0),
+        *_make_differences(1),
+        *_make_differences(2),
+        *_make_differences(3),
+        *_make_differences(4),
+    ]
 
 
 def main() -> int:
@@ -25,6 +66,13 @@ def main() -> int:
         type=str,
         help='Write patches to a file instead of stdout.',
     )
+    parser.add_argument(
+        '--mode',
+        type=str,
+        choices=["legacy", "standard"],
+        default="standard",
+        help="Mode to use when creating the patch. Defaults to standard mode (header modifications only).",
+    )
 
     # Grab what we're doing
     args = parser.parse_args()
@@ -32,8 +80,12 @@ def main() -> int:
     # Grab the rom, parse it
     with open(args.bin, "rb") as fp:
         data = fp.read()
-    newdata = force_no_attract_sound(data)
-    differences = ["# Description: force silent attract mode", *Binary.diff(data, newdata)]
+    if args.mode == "legacy":
+        differences = legacy_force_no_attract(data)
+    elif args.mode == "standard":
+        differences = standard_force_no_attract(data)
+    else:
+        raise Exception(f"Invalid choice {args.mode}")
     if not args.patch_file:
         for line in differences:
             print(line)
