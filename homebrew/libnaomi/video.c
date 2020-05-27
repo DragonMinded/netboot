bc8921f3d94551c1c5da8bd6e8e69f5c2e923f59
Jennifer Taylor <dragonminded@dragonminded.com>
2020-05-27 21:12:51 +0000

    Fix previously broken build, thought I compiled it but I didn't.

--- homebrew/libnaomi/video.c
+++ homebrew/libnaomi/video.c
@@ -7,6 +7,7 @@
 
 // TODO: Need to support more than 640x480 framebuffer mode.
 // TODO: Need to support more than RGB565 color.
+// TODO: Need to support vertical orientation.
 
 
 static void *buffer_base = 0;
@@ -17,8 +18,8 @@ void video_wait_for_vblank()
 {
     volatile unsigned int *videobase = (volatile unsigned int *)POWERVR2_BASE;
 
-    while(!(videobase[SYNC_STAT] & 0x01ff)) { ; }
-    while((videobase[SYNC_STAT] & 0x01ff)) { ; }
+    while(!(videobase[POWERVR2_SYNC_STAT] & 0x01ff)) { ; }
+    while((videobase[POWERVR2_SYNC_STAT] & 0x01ff)) { ; }
 }
 
 unsigned int video_width()
@@ -45,66 +46,65 @@ void video_init_simple()
     volatile unsigned int *videobase = (volatile unsigned int *)POWERVR2_BASE;
 
     // Set up video timings copied from Naomi BIOS.
-    videobase[VRAM_CFG3] = 0x15D1C955;
-    videobase[VRAM_CFG1] = 0x00000020;
+    videobase[POWERVR2_VRAM_CFG3] = 0x15D1C955;
+    videobase[POWERVR2_VRAM_CFG1] = 0x00000020;
 
     // Reset video.
-    videobase[RESET] = 0;
+    videobase[POWERVR2_RESET] = 0;
 
     // Set border color to black.
-    videobase[BORDER_COL] = 0;
+    videobase[POWERVR2_BORDER_COL] = 0;
 
     // Don't display border across whole screen.
-    videobase[VIDEO_CFG] = 0x00160000;
+    videobase[POWERVR2_VIDEO_CFG] = 0x00160000;
 
     // Set up frameebuffer config to enable display, set pixel mode, no line double.
-    videobase[FB_DISPLAY_CFG] = (
+    videobase[POWERVR2_FB_DISPLAY_CFG] = (
         0x1 << 23 |  // Double pixel clock for VGA.
         0x1 << 2 |   // RGB565 mode.
         0x1 << 0     // Enable display.
     );
 
     // Set up framebuffer render config to dither enabled, RGB565, no alpha threshold.
-    videobase[FB_RENDER_CFG] = (
+    videobase[POWERVR2_FB_RENDER_CFG] = (
         0x1 << 3 |  // Dither enabled.
         0x1 << 0    // RGB565 mode.
     );
 
     // Set up even/odd field video base address, shifted by bpp.
-    buffer_loc = 1;
     video_display();
 
     // Set up render modulo, (bpp * width) / 8.
-    videobase[FB_RENDER_MODULO] = (2 * 640) / 8;
+    videobase[POWERVR2_FB_RENDER_MODULO] = (2 * 640) / 8;
 
     // Set up vertical position.
-    videobase[VPOS] = (
+    videobase[POWERVR2_VPOS] = (
         35 << 16 |  // Even position.
         35 << 0     // Odd position.
     );
-    videobase[VBORDER] = (
+    videobase[POWERVR2_VBORDER] = (
         40 << 16 |  // Start.
         (480 + 40) << 0    // End.
     );
 
     // Set up horizontal position.
-    videobase[HPOS] = 166;
+    videobase[POWERVR2_HPOS] = 166;
 
     // Set up refresh rate.
-    videobase[SYNC_LOAD] = (
+    videobase[POWERVR2_SYNC_LOAD] = (
         524 << 16  |  // Vsync
         857 << 0      // Hsync
     );
 
     // Set up display size.
-    videobase[FB_DISPLAY_SIZE] = (
+    videobase[POWERVR2_FB_DISPLAY_SIZE] = (
         1 << 20 |                   // Interlace skip modulo if we are interlaced ((width / 4) * bpp) + 1
         (480 - 1) << 10 |           // height - 1
         (((640 / 4) * 2) - 1) << 0  // ((width / 4) * bpp) - 1
     );
 
     // Enable display
-    videobase[SYNC_CFG] = (
+    videobase[POWERVR2_SYNC_CFG] = (
         1 << 8 |  // Enable video
         0 << 6 |  // VGA mode
         0 << 4 |  // Non-interlace
@@ -113,10 +113,10 @@ void video_init_simple()
     );
 
     // Set up horizontal clipping to clip within 0-640.
-    videobase[FB_CLIP_X] = (640 << 16) | (0 << 0);
+    videobase[POWERVR2_FB_CLIP_X] = (640 << 16) | (0 << 0);
 
     // Set up vertical clipping to within 0-480.
-    videobase[FB_CLIP_Y] = (480 << 16) | (0 << 0);
+    videobase[POWERVR2_FB_CLIP_Y] = (480 << 16) | (0 << 0);
 
     // Wait for vblank like games do.
     video_wait_for_vblank();
@@ -314,9 +314,17 @@ void video_draw_text( int x, int y, uint32_t color, const char * const msg )
 
 void video_display()
 {
+    volatile unsigned int *videobase = (volatile unsigned int *)POWERVR2_BASE;
+    uint32_t buffer_offset[2] = {
+        0,
+        video_width() * video_height() * video_depth(),
+    };
+
     // Swap buffers in HW
-    buffer_loc = 1 - buffer_loc;
-    videobase[FB_DISPLAY_ADDR_1] = (640 * 480 * 2 * (1 - buffer_loc)) << 1;
-    videobase[FB_DISPLAY_ADDR_2] = (640 + (640 * 480 * 2 * (1 - buffer_loc))) << 1;
-    buffer_base = (VRAM_BASE + (640 * 480 * 2)) * buffer_loc;
+    videobase[POWERVR2_FB_DISPLAY_ADDR_1] = buffer_offset[buffer_loc];
+    videobase[POWERVR2_FB_DISPLAY_ADDR_2] = buffer_offset[buffer_loc] + (video_width() * video_depth());
+
+    // Swap buffer pointer in SW
+    buffer_loc = buffer_loc ? 0 : 1;
+    buffer_base = (void *)(VRAM_BASE + buffer_offset[buffer_loc]);
 }
