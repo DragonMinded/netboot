40784914b14d75d61c45554df3d4094562fb5fb8
Jennifer Taylor <dragonminded@dragonminded.com>
2020-05-28 20:35:08 +0000

    Set up base environment for compiling ARM code for the AICA.

--- homebrew/setup/env.sh
+++ homebrew/setup/env.sh
@@ -2,7 +2,7 @@
 
 export NAOMI_BASE=/opt/toolchains/naomi
 export NAOMI_SH_BASE=${NAOMI_BASE}/sh-elf
-export NAOMI_ARM_BASE=${NAOMI_BASE}/arm-eab
+export NAOMI_ARM_BASE=${NAOMI_BASE}/arm-eabi
 export NAOMI_SH_GCC_VER=9.3.0
 export NAOMI_ARM_GCC_VER=8.4.0
 
@@ -13,3 +13,10 @@ export NAOMI_SH_AS=${NAOMI_SH_BASE}/bin/sh-elf-as
 export NAOMI_SH_AR=${NAOMI_SH_BASE}/bin/sh-elf-ar
 export NAOMI_SH_OBJCOPY=${NAOMI_SH_BASE}/bin/sh-elf-objcopy
 export NAOMI_SH_OBJDUMP=${NAOMI_SH_BASE}/bin/sh-elf-objdump
+
+export NAOMI_ARM_CC=${NAOMI_ARM_BASE}/bin/arm-eabi-gcc
+export NAOMI_ARM_LD=${NAOMI_ARM_BASE}/bin/arm-eabi-ld
+export NAOMI_ARM_AS=${NAOMI_ARM_BASE}/bin/arm-eabi-as
+export NAOMI_ARM_AR=${NAOMI_ARM_BASE}/bin/arm-eabi-ar
+export NAOMI_ARM_OBJCOPY=${NAOMI_ARM_BASE}/bin/arm-eabi-objcopy
+export NAOMI_ARM_OBJDUMP=${NAOMI_ARM_BASE}/bin/arm-eabi-objdump
