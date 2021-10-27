9777f9aaf41f3df45083bffb2388e30807772e4a
Jennifer Taylor <dragonminded@dragonminded.com>
2021-10-27 04:41:01 +0000

    Fix incorrect link to settings file.

--- services/menu.sh
+++ services/menu.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-/home/pi/netboot/netdimm_menu 192.168.1.2 /boot/roms/ --menu-settings-file /boot/roms/netdimm_menu_settings.yaml --persistent
+/home/pi/netboot/netdimm_menu 192.168.1.2 /boot/roms/ --menu-settings-file /boot/netdimm_menu_settings.yaml --persistent
