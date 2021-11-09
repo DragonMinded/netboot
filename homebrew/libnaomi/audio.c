1c4582457dd6d909ea6831d4c2069cb27688a6b2
Jennifer Taylor <dragonminded@dragonminded.com>
2021-11-09 00:28:46 +0000

    Fix internal argument handling for param exchange.

--- homebrew/libnaomi/audio.c
+++ homebrew/libnaomi/audio.c
@@ -91,7 +91,7 @@ uint32_t audio_aica_uptime()
     return AICA_CMD_BUFFER(CMD_BUFFER_UPTIME);
 }
 
-uint32_t __audio_exchange_command(uint32_t command, uint32_t *params)
+uint32_t __audio_exchange_command(uint32_t command, uint32_t param1, uint32_t param2, uint32_t param3, uint32_t param4)
 {
     // Make sure we have room in the FIFO to read busy register.
     __aica_fifo_wait();
@@ -102,16 +102,12 @@ uint32_t __audio_exchange_command(uint32_t command, uint32_t *params)
     // Make sure we have room in the FIFO to write the command.
     __aica_fifo_wait();
 
-    // Set up the command and registers.
+    // Set up the command and param registers.
     AICA_CMD_BUFFER(CMD_BUFFER_REQUEST) = command;
-
-    if (params)
-    {
-        for (int param = 0; param < 4; param ++)
-        {
-            AICA_CMD_BUFFER(CMD_BUFFER_PARAMS + (param * 4)) = params[param];
-        }
-    }
+    AICA_CMD_BUFFER(CMD_BUFFER_PARAMS + 0x0) = param1;
+    AICA_CMD_BUFFER(CMD_BUFFER_PARAMS + 0x4) = param2;
+    AICA_CMD_BUFFER(CMD_BUFFER_PARAMS + 0x8) = param3;
+    AICA_CMD_BUFFER(CMD_BUFFER_PARAMS + 0xC) = param4;
 
     // Trigger the AICA to react to the command.
     AICA_CMD_BUFFER(CMD_BUFFER_BUSY) = 1;
@@ -144,19 +140,20 @@ void audio_free()
     if (initialized)
     {
         // Kill all running sounds.
-        __audio_exchange_command(REQUEST_SILENCE, 0);
+        __audio_exchange_command(REQUEST_SILENCE, 0, 0, 0, 0);
         initialized = 0;
     }
 }
 
 uint32_t __audio_get_location(int format, unsigned int samplerate, unsigned int num_samples)
 {
-    uint32_t params[4] = {
+    return __audio_exchange_command(
+        REQUEST_ALLOCATE,
         num_samples,
         format == AUDIO_FORMAT_16BIT ? ALLOCATE_AUDIO_FORMAT_16BIT : ALLOCATE_AUDIO_FORMAT_8BIT,
         samplerate,
-    };
-    return __audio_exchange_command(REQUEST_ALLOCATE, params);
+        0
+    );
 }
 
 uint32_t __audio_load_location(int format, unsigned int samplerate, void *data, unsigned int num_samples)
@@ -177,24 +174,23 @@ int audio_play_sound(int format, unsigned int samplerate, uint32_t speakers, voi
     uint32_t location = __audio_load_location(format, samplerate, data, num_samples);
     if (location != 0)
     {
-        uint32_t params[4] = {location, 0};
+        uint32_t panning = 0;
         if (speakers & SPEAKER_LEFT)
         {
-            params[1] |= ALLOCATE_SPEAKER_LEFT;
+            panning |= ALLOCATE_SPEAKER_LEFT;
         }
         if (speakers & SPEAKER_RIGHT)
         {
-            params[1] |= ALLOCATE_SPEAKER_RIGHT;
+            panning |= ALLOCATE_SPEAKER_RIGHT;
         }
 
-        // Technically this only takes one parameter, but whatever.
-        if (__audio_exchange_command(REQUEST_DISCARD_AFTER_USE, params) != RESPONSE_SUCCESS)
+        if (__audio_exchange_command(REQUEST_DISCARD_AFTER_USE, location, 0, 0, 0) != RESPONSE_SUCCESS)
         {
             return -1;
         }
 
         // Now, play the track.
-        return __audio_exchange_command(REQUEST_START_PLAY, params) == RESPONSE_SUCCESS ? 0 : -1;
+        return __audio_exchange_command(REQUEST_START_PLAY, location, panning, 0, 0) == RESPONSE_SUCCESS ? 0 : -1;
     }
     else
     {
@@ -220,8 +216,7 @@ void audio_unregister_sound(int sound)
 {
     if (sound > 0)
     {
-        uint32_t params[4] = {sound};
-        __audio_exchange_command(REQUEST_FREE, params);
+        __audio_exchange_command(REQUEST_FREE, sound, 0, 0, 0);
     }
 }
 
@@ -229,17 +224,17 @@ int audio_play_registered_sound(int sound, uint32_t speakers)
 {
     if (sound > 0)
     {
-        uint32_t params[4] = {sound, 0};
+        uint32_t panning = 0;
         if (speakers & SPEAKER_LEFT)
         {
-            params[1] |= ALLOCATE_SPEAKER_LEFT;
+            panning |= ALLOCATE_SPEAKER_LEFT;
         }
         if (speakers & SPEAKER_RIGHT)
         {
-            params[1] |= ALLOCATE_SPEAKER_RIGHT;
+            panning |= ALLOCATE_SPEAKER_RIGHT;
         }
 
-        return __audio_exchange_command(REQUEST_START_PLAY, params) == RESPONSE_SUCCESS ? 0 : -1;
+        return __audio_exchange_command(REQUEST_START_PLAY, sound, panning, 0, 0) == RESPONSE_SUCCESS ? 0 : -1;
     }
     else
     {
