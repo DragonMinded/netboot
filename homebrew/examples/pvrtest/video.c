cd6693e13b96fe2f155a2bce29002f797869fca4
Jennifer Taylor <dragonminded@dragonminded.com>
2021-11-15 17:13:12 +0000

    Get rid of video register overrides that were messing with our own setup/teardown.

--- homebrew/examples/pvrtest/video.c
+++ homebrew/examples/pvrtest/video.c
@@ -33,28 +33,6 @@ static unsigned int three_d_params[] = {
 	0x8108, 0x00000003      /* 32bit palette  */
 };
 
-static unsigned int scrn_params[] = {
-	0x80e8, 0x00160000,	/* screen control */
-	0x8044, 0x00800000,	/* pixel mode (vb+0x11) */
-	0x805c, 0x00000000,	/* Size modulo and display lines (vb+0x17) */
-	0x80d0, 0x00000100,	/* interlace flags */
-	0x80d8, 0x020c0359,	/* M */
-	0x80cc, 0x001501fe,	/* M */
-	0x80d4, 0x007e0345,	/* horizontal border */
-	0x80dc, 0x00240204,	/* vertical position */
-	0x80e0, 0x07d6c63f,	/* sync control */
-	0x80ec, 0x000000a4,	/* horizontal position */
-	0x80f0, 0x00120012,	/* vertical border */
-	0x80c8, 0x03450000,	/* set to same as border H in 80d4 */
-	0x8068, 0x027f0000,	/* (X resolution - 1) << 16 */
-	0x806c, 0x01df0000,	/* (Y resolution - 1) << 16 */
-	0x804c, 0x000000a0,	/* display align */
-	0x8118, 0x00008040,	/* M */
-	0x80f4, 0x00000401,	/* anti-aliasing */
-	0x8048, 0x00000009,	/* alpha config */
-	0x8040, 0x00000000	/* border color */
-};
-
 static void set_regs(unsigned int *values, int cnt)
 {
   volatile unsigned char *regs = (volatile unsigned char *)(void *)0xa05f0000;
@@ -72,7 +50,7 @@ void init_pvr()
   volatile unsigned int *vbl = (volatile unsigned int *)(void *)0xa05f810c;
 
   set_regs(three_d_params, sizeof(three_d_params)/sizeof(three_d_params[0])/2);
+  // Wait for vblank.
   while (!(*vbl & 0x01ff));
   while (*vbl & 0x01ff);
-  set_regs(scrn_params, sizeof(scrn_params)/sizeof(scrn_params[0])/2);
 }
