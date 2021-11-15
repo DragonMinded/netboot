e7bde1ea7829842a239d20c74e42739c4077fa54
Jennifer Taylor <dragonminded@dragonminded.com>
2021-11-15 17:27:19 +0000

    Remove more register squashing from PVR test video setup.

--- homebrew/examples/pvrtest/video.c
+++ homebrew/examples/pvrtest/video.c
@@ -5,32 +5,24 @@
 /* These values mainly from Dans 3dtest program... */
 
 static unsigned int three_d_params[] = {
-	0x80a8, 0x15d1c951,	/* M (Unknown magic value) */
-	0x80a0, 0x00000020,	/* M */
-	0x8008, 0x00000000,	/* TA out of reset */
-	0x8048, 0x00000009,	/* alpha config */
-	0x8068, 0x02800000,	/* pixel clipping x */
-	0x806c, 0x01e00000,	/* pixel clipping y */
-	0x8110, 0x00093f39,	/* M */
-	0x8098, 0x00800408,	/* M */
-	0x804c, 0x000000a0,	/* display align (640*2)/8 */
-	0x8078, 0x3f800000,	/* polygon culling (1.0f) */
-	0x8084, 0x00000000,	/* M */
-	0x8030, 0x00000101,	/* M */
-	0x80b0, 0x007f7f7f,	/* Fog table color */
-	0x80b4, 0x007f7f7f,	/* Fog vertex color */
-	0x80c0, 0x00000000,	/* color clamp min */
-	0x80bc, 0xffffffff,	/* color clamp max */
-	0x8080, 0x00000007,	/* M */
-	0x8074, 0x00000001,	/* cheap shadow */
-	0x807c, 0x0027df77,	/* M */
+	0x8098, 0x00800408,	/* Polygon sorting and cache sizes */
+	0x8078, 0x3f800000,	/* Polygon culling (1.0f) */
+	0x8084, 0x00000000,	/* Perpendicular triangle compare (0.0f) */
+	0x8030, 0x00000101,	/* Span sorting enable */
+	0x80b0, 0x007f7f7f,	/* Fog table color (ARGB, A is ignored) */
+	0x80b4, 0x007f7f7f,	/* Fog vertex color (ARGB, A is ignored) */
+	0x80c0, 0x00000000,	/* Color clamp min (ARGB) */
+	0x80bc, 0xffffffff,	/* Color clamp max (ARGB) */
+	0x8080, 0x00000007,	/* Pixel sampling position, everything set at (0.5, 0.5) */
+	0x8074, 0x00000000,	/* Shadow scaling */
+	0x807c, 0x0027df77,	/* FPU params? */
 	0x8008, 0x00000001,	/* TA reset */
 	0x8008, 0x00000000,	/* TA out of reset */
-	0x80e4, 0x00000000,	/* stride width */
+	0x80e4, 0x00000000,	/* stride width (TSP_CFG) */
 	0x80b8, 0x0000ff07,	/* fog density */
 	0x80b4, 0x007f7f7f,	/* fog vertex color */
 	0x80b0, 0x007f7f7f,	/* fog table color */
-	0x8108, 0x00000003      /* 32bit palette  */
+	0x8108, 0x00000003  /* 32bit palette (0x0 = ARGB1555, 0x1 = RGB565, 0x2 = ARGB4444, 0x3 = ARGB8888  */
 };
 
 static void set_regs(unsigned int *values, int cnt)
